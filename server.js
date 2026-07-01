c182f9bfd7cb95a67e726ae937899c6f05dc26ed
DIVYA RAJ <divyaraj6952@gmail.com>
2026-07-01 12:41:30 +0530

    fix routes path issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 const { connectDB } = require("./config/db");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const startserver = async () => {
     try{
